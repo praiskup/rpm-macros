2c8674c5f102887d6a62d63d33b1deb4b9a1203b
Pavel Raiskup <pavel@raiskup.cz>
2025-10-05 23:20:34 +0200

    extract_arch_info.py: extract architecture info from a specfile
    ./extract_arch_info.py --targets fedora-rawhide-x86_64 --specfile foo.spec --targets fedora-rawhide-s390x --targets rhel-7-x86_64 --targets fedora-42-x86_64
    fedora-42-x86_64:
      exclusivearch: i386 i486 i586 i686 pentium3 pentium4 athlon geode x86_64 armv3l
        armv4b armv4l armv4tl armv5tl armv5tel armv5tejl armv6l armv6hl armv7l armv7hl
        armv7hnl armv8l armv8hl armv8hnl armv8hcnl aarch64 ppc64le s390x riscv64
    fedora-rawhide-s390x:
      excludearch: x86_64
      exclusivearch: i386 i486 i586 i686 pentium3 pentium4 athlon geode x86_64 armv3l
        armv4b armv4l armv4tl armv5tl armv5tel armv5tejl armv6l armv6hl armv7l armv7hl
        armv7hnl armv8l armv8hl armv8hnl armv8hcnl aarch64 ppc64le s390x riscv64
    fedora-rawhide-x86_64:
      excludearch: x86_64
      exclusivearch: i386 i486 i586 i686 pentium3 pentium4 athlon geode x86_64 armv3l
        armv4b armv4l armv4tl armv5tl armv5tel armv5tejl armv6l armv6hl armv7l armv7hl
        armv7hnl armv8l armv8hl armv8hnl armv8hcnl aarch64 ppc64le s390x riscv64
    rhel-7-x86_64:
      excludearch: x86_64

---
+++ extract_arch_info.py
@@ -0,0 +1,82 @@
+#! /bin/python3
+
+import argparse
+import copy
+import os
+
+import yaml
+
+from norpm.macrofile import system_macro_registry
+from norpm.specfile import ParserHooks, specfile_expand
+
+
+class Hooks(ParserHooks):
+    """ Gather access to spec tags """
+    def __init__(self):
+        self.tags = {}
+    def tag_found(self, name, value, _tag_raw):
+        """ Gather EclusiveArch, ExcludeArch, BuildArch... """
+        if name.lower() in ["exclusivearch", "excludearch", "buildarch"]:
+            self.tags[name] = value
+
+
+def _get_override_db():
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    file = os.path.join(dirname, "data-builder-architecture.yaml")
+    with open(file, 'r', encoding="utf8") as f:
+        file_data = yaml.safe_load(f)
+    return file_data
+
+
+def _get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--specfile", required=True)
+    excl = parser.add_mutually_exclusive_group()
+    excl.add_argument("--target")
+    excl.add_argument("--targets", action="append")
+    return parser
+
+
+def handle_target(origdb, overrides, specfile, target):
+    """
+    Using the system database ORIGDB, and OVERRIDES, expand SPECFILE
+    for Mock TARGET.
+    """
+    db = copy.deepcopy(origdb)
+    distro, _ = target.rsplit("-", 1)
+    for key in overrides.keys():
+        db.undefine(key)
+        for data in overrides[key]:
+            if distro in data["distro"]:
+                if data["value"] is None:
+                    continue
+                value = data["value"][-1]
+                if value["params"] is None:
+                    db.define(key, value["def"])
+                else:
+                    db.define(key, (value["def"], value["params"]))
+
+    hooks = Hooks()
+    with open(specfile, "r", encoding="utf8") as fd:
+        specfile_expand(fd.read(), db, hooks)
+    return hooks.tags
+
+
+def _main():
+    opts = _get_arg_parser().parse_args()
+    db = system_macro_registry()
+    db["dist"] = ""
+    db.known_norpm_hacks()
+    overrides = _get_override_db()
+
+    if opts.target:
+        result = handle_target(db, overrides, opts.specfile, opts.target)
+    else:
+        result = {}
+        for target in opts.targets:
+            result[target] = handle_target(db, overrides, opts.specfile, target)
+
+    print(yaml.dump(result, sort_keys=True))
+
+if __name__ == "__main__":
+    _main()
